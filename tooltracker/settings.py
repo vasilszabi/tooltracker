4e6e2db83a4815c6455e5596e3b4a500867e4b21
vasilszabi <szabolcs.vasil@gmail.com>
2025-08-08 16:55:52 +0200

    Add Procfile, update ALLOWED_HOSTS, add gunicorn and whitenoise

--- tooltracker/settings.py
+++ tooltracker/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1234567890demo'
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # APLIKÁCIE
 INSTALLED_APPS = [
