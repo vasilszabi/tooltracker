d1390ecfdc9049ce9e1177f83c42e7991b47ee38
vasilszabi <szabolcs.vasil@gmail.com>
2025-08-08 17:13:27 +0200

    Auto-create admin user on deploy

--- manage.py
+++ manage.py
@@ -5,5 +5,9 @@ from django.core.management import execute_from_command_line
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tooltracker.settings')
 
 if __name__ == '__main__':
+    from django.contrib.auth.models import User
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "admin123")
+
     execute_from_command_line(sys.argv)
 
